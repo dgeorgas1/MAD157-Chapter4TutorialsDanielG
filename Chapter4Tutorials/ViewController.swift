a2a261e26210acb535c70bfdefb9b935819dab50
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-02-12 20:02:09 -0600

    Commit changes

--- Chapter4Tutorials/ViewController.swift
+++ Chapter4Tutorials/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: Methods
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textDisplay.text?.uppercased()
+        print(labelResult.text!)
+    }
 }
-
